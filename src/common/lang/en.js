fefee11c48a9754f0037de92865ee8e0f9f2d33a
Maxwell-chy <80674872+Maxwell-chy@users.noreply.github.com>
2022-06-14 17:38:43 +0800

    0
    0

--- src/common/lang/en.js
+++ src/common/lang/en.js
@@ -1,15 +1,4 @@
 export const m = {
-  text:'Taihe hall, also known as "Jinluan hall, Supreme Jinluan hall and Jinluan treasure hall", ' +
-    'is located in the Palace Museum at No. 4 jingshanqian street, Dongcheng District, Beijing. ' +
-    'It stands in the center of the Forbidden City, with a construction area of 2377 square meters.' +
-    ' It is the largest palace in the Forbidden City (the Forbidden City) and one of the three Oriental halls. ' +
-    'The meridian of the central axis of the capital rises up to the three platforms along the dragon stone ' +
-    'carving imperial road and passes under the throne of the emperor. It is the most regulated ancient palace ' +
-    'building in China and the place where the emperor holds major imperial ceremonies. The hall is decorated ' +
-    'with thousands of golden dragon patterns inside and outside, and ten ridge animals are installed at the ' +
-    'ridge corner, which is the only example in the existing ancient buildings. The hall of Supreme Harmony was ' +
-    'built in 1420, the 18th year of Yongle in the Ming Dynasty. It was originally named Fengtian hall, and has ' +
-    'been damaged and rebuilt several times since then; Ming Jiajing Dynasty renamed Huangji hall; After the Manchu ' +
-    'and Qing Dynasties established their capital in Beijing, it was changed to its present name; The present Hall ' +
-    'of Supreme Harmony was rebuilt and preserved in the Kangxi years of the Qing Dynasty.'
+  text:'Taihe hall, located in the Palace Museum and standing in the center of the Forbidden City, is one of the three great halls in the East. It is the most regulated ancient palace building in China and the place where the emperor held major ceremonies.  The hall of Supreme Harmony was completed in 1420. The hall of Supreme Harmony was rebuilt and preserved in the reign of Emperor Kangxi of the Qing Dynasty.',
+  garden:'The imperial garden is located on the central axis of the Forbidden City in Beijing. It was called the Palace back garden in the Ming Dynasty. Founded in 1420, it still retains the basic pattern when it was first built. Qin\'an hall, the main building in the park, is of the double eaved roof type. With it as the center, pavilions and pavilions are spread to the front and both sides. The garden forms an evergreen landscape.',
 }
